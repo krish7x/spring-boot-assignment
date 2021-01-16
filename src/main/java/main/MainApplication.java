7a3058130631a64fd691cbc0b2450052b8a01523
krish7x <krishnakumar99g@gmail.com>
2021-01-17 01:04:48 +0530

    hash map added

--- src/main/java/main/MainApplication.java
+++ src/main/java/main/MainApplication.java
@@ -16,9 +16,7 @@ public class MainApplication  {
     private static final String FILE_NAME = Constants.file1;
     public static  String corp_name;
     public static  List<String> result = new ArrayList<String>();
-    public static String[] arr = {};
     public static final String file3 = Constants.file3;
-
 
     public static void main(String[] args) throws IOException, InvalidFormatException {
 
@@ -33,30 +31,57 @@ public class MainApplication  {
         // Creating DataFormatter to format and get each cell's value as String
         DataFormatter dataFormatter = new DataFormatter();
 
-        //obtaining a rowIterator and columnIterator and iterate over them
+        //Obtaining a rowIterator and columnIterator and iterate over them
         System.out.println("\n\nIterating over Rows and Columns using Iterator\n");
         Iterator<Row> rowIterator = sheet.rowIterator();
+
+        // Creating payment details of all individuals and headerTabs
+        List<String> headerTabs = new ArrayList<String>();
+        HashMap<String, HashMap> payment_details = new HashMap<String, HashMap>();
+
+        // Variable to denote first iteration to fetch header Tabs
+        Boolean firstInitiative = true;
+
+
         while (rowIterator.hasNext()) {
             Row row = rowIterator.next();
 
             //iterate over the columns of the current row
             Iterator<Cell> cellIterator = row.cellIterator();
 
+            // Hashmap to store each individual
+            HashMap<String, String> personPaymentDetails = new HashMap<String, String>();
+
+            // re-Initiating notation variables
+            boolean userIdObtained = false;
+            Integer rowLength = 0;
+            String userId = "1";
+
             while (cellIterator.hasNext()) {
                 Cell cell = cellIterator.next();
                 String cellValue = dataFormatter.formatCellValue(cell);
                 System.out.print(cellValue + "\t");
                 result.add(cellValue + ",");
-
+                if (firstInitiative) { // used to fetch the header cells
+                    headerTabs.add(cellValue);
+                } else {
+                    if (!userIdObtained) { // used to obtain the userId for a individual
+                        userId = cellValue;
+                        userIdObtained = true;
+                    }
+                    personPaymentDetails.put(headerTabs.get(rowLength++),cellValue); //assigning the cells to individual hashmap
+                }
             }
-
+            firstInitiative = false;
+            payment_details.put(userId,personPaymentDetails); // mapping individual hashmap to overall hashmap
             System.out.println();
         }
 
         //printing the list
-//        for(String ans: result)
-//            System.out.print(ans);
-
+        for(String userId: payment_details.keySet()) {
+            HashMap userDetails = payment_details.get(userId);
+            System.out.println(userDetails);
+        }
 
         List<String>sub_list = result.subList(6,result.size());
         //sub_list.remove(sub_list.size() - 1);
@@ -70,7 +95,7 @@ public class MainApplication  {
 
         //----------------------
         //gives file name
-        String corp_file = charRemoveAt(FILE_NAME, 15);
+        String corp_file = charRemoveAt(FILE_NAME, 24);
 
         System.out.println("\n" + "\n" + "File Name --> " +corp_file);
 
